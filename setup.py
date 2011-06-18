4a6514df30fd9ea76519c6dabcbcc397302add78
Patrick Altman <paltman@gmail.com>
2011-06-18 15:42:49 -0500

    0.1.dev1003

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-boxes",
-    version = "0.1.dev1002",
+    version = "0.1.dev1003",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "a reusable Django content-boxes application",
