b39671f27d07cef1a190317b6d45314022f412d0
James Tauber <jtauber@jtauber.com>
2014-07-31 19:20:58 -0400

    flake8 fixes

--- boxes/urls.py
+++ boxes/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import url, patterns
 
 
-urlpatterns = patterns("boxes.views",
+urlpatterns = patterns(
+    "boxes.views",
     url(r"^([-\w]+)/edit/$", "box_edit", name="box_edit"),
 )
--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 
 
 setup(
-    name = "django-boxes",
-    version = "2.0b4",
-    author = "Eldarion",
-    author_email = "development@eldarion.com",
-    description = "a reusable Django content-boxes application",
-    long_description = open("README.rst").read(),
-    license = "BSD",
-    url = "http://github.com/eldarion/boxes",
-    packages = [
+    name="django-boxes",
+    version="2.0b4",
+    author="Eldarion",
+    author_email="development@eldarion.com",
+    description="a reusable Django content-boxes application",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/eldarion/boxes",
+    packages=[
         "boxes",
         "boxes.templatetags",
     ],
-    classifiers = [
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
